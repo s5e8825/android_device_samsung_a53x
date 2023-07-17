4dac7e3b2760b5cb9657edf8f0bf313b00108ae9
KrutosVIP <52695559+KrutosVIP@users.noreply.github.com>
2023-07-17 11:20:21 +0000

    a53x: Make vendor_boot buildable
    - also fix dts with current CIS kernel

--- Android.mk
+++ Android.mk
@@ -18,4 +18,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter a53x, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+RECOVERY_KERNEL_MODULES := $(addprefix $(TARGET_RECOVERY_ROOT_OUT)/lib/modules/,$(shell cat device/samsung/a53x/modules.load))
+
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+INTERNAL_RECOVERY_RAMDISK_FILES_TIMESTAMP := $(call intermediates-dir-for,PACKAGING,recovery)/ramdisk_files-timestamp
+
+$(RECOVERY_KERNEL_MODULES): $(INSTALLED_KERNEL_TARGET)
+	@echo "Copying kernel modules to recovery ramdisk: $@"
+	@mkdir -p $(dir $@)
+	cp -r $(TARGET_OUT_VENDOR_DLKM)/lib/ $(TARGET_RECOVERY_ROOT_OUT)/
+
+$(INTERNAL_RECOVERY_RAMDISK_FILES_TIMESTAMP): $(RECOVERY_KERNEL_MODULES)
 endif
